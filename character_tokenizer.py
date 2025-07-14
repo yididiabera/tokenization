99082b68ccf203fa6860301107c3fe60ea4d26c0
Jedidiah Abera <yididiabera87@gmail.com>
2025-07-14 12:13:50 +0300

    Implement a character tokenizer

---
+++ character_tokenizer.py
@@ -0,0 +1,13 @@
+def character_tokenizer(text):
+    " A character-level tokenizer that splits text into individual characters."
+    
+    token = list(text)
+    
+    return token
+
+# Example 
+input = "I'm not in danger. I'm the danger!"
+tokens = character_tokenizer(input)
+
+print("Original text:", input)
+print("Character tokens:", tokens)
